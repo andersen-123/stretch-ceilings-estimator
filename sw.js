3e93153518a76f4ddb0ab8f30b8f5271df436dcb
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-18 16:41:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,26 @@
-const CACHE_NAME = 'stretch-ceilings-cache-v1';
-const FILES_TO_CACHE = [
-  '/',
-  'index.html',
-  'style.css',
-  'app.js',
-  'pdf-generator.js',
-  'icons/icon-192.png',
-  'icons/icon-512.png',
-  'data/default-items.json',
-  'data/default-templates.json',
-  'data/settings.json',
-  'data/company-info.json'
+const CACHE_NAME = 'estimator-v1';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './pdf-generator.js',
+  './icons/icon-192.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
